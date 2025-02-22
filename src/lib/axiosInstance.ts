82311366685acefcc275a087713e27e59af057b8
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-22 01:02:03 +0000

    Feb 22, 2025, 2:02 AM

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -5,7 +5,6 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "multipart/form-data",
     Accept: "application/json",
-    "X-Requested-With": "XMLHttpRequest",
   },
 });
 
