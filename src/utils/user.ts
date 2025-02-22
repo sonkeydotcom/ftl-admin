e0c7c42a78bca63bd039461b09e35427059c4e18
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-22 05:56:25 +0000

    Feb 22, 2025, 6:56 AM

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axiosInstance from "../lib/axiosInstance";
 
 export const fetchUserNameById = async (userId: string) => {
   try {
-    const response = await axios.get(`/api/admin/user/${userId}`); // Modify this URL based on your backend
+    const response = await axiosInstance.get(`/api/admin/user/${userId}`); // Modify this URL based on your backend
     return response.data.name; // assuming the response contains a `username` field
   } catch (error) {
     console.error("Error fetching username:", error);
